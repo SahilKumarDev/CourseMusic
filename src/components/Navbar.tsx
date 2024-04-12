4efef548145bb0b5bf43d91994b4b30d303951d2
NSxVillan <Sahilkumar710@outlook.com>
2024-04-12 06:55:11 +0530

    Day_2

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client"; // to use react hook for
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        {/* <MenuItem setActive={setActive} active={active} item="Home"></MenuItem> */}
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="#">All Courses</HoveredLink>
+            <HoveredLink href="#">Basic Music Theory</HoveredLink>
+            <HoveredLink href="#">Advacned Compostition</HoveredLink>
+            <HoveredLink href="#">Song Writing</HoveredLink>
+            <HoveredLink href="#">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+<Link href={"/contact"}>
+
+<MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+</Link>
+
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
