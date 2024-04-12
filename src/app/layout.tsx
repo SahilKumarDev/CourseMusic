50f1087d76d81c8ccaa334d4ed4cdc3c4d9297cd
NSxVillan <Sahilkumar710@outlook.com>
2024-04-12 18:35:26 +0530

    Bug Fixes for nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import favicon from "./favicon.png"
 
-// Compnents
+// NavBar Compnents
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,12 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        {/* Navbar */}
         <div className="relative w-full flex items-center justify-center">
-        <Navbar/>
+          <Navbar />
         </div>
 
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import { cn } from "@/utils/cn";
-import Image from "next/image";
 import React, {
   createContext,
   useState,
@@ -45,6 +42,19 @@ export const CardContainer = ({
     setIsMouseEntered(false);
     containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
   };
+
+  useEffect(() => {
+    const handleAnimations = () => {
+      if (!containerRef.current) return;
+      if (isMouseEntered) {
+        containerRef.current.style.transform = "your transformation here";
+      } else {
+        containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
+      }
+    };
+    handleAnimations();
+  }, [isMouseEntered]);
+
   return (
     <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
       <div
@@ -122,17 +132,16 @@ export const CardItem = ({
   const [isMouseEntered] = useMouseEnter();
 
   useEffect(() => {
+    const handleAnimations = () => {
+      if (!ref.current) return;
+      if (isMouseEntered) {
+        ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
+      } else {
+        ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+      }
+    };
     handleAnimations();
-  }, [isMouseEntered]);
-
-  const handleAnimations = () => {
-    if (!ref.current) return;
-    if (isMouseEntered) {
-      ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
-    } else {
-      ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
-    }
-  };
+  }, [isMouseEntered, translateX, translateY, translateZ, rotateX, rotateY, rotateZ]);
 
   return (
     <Tag
